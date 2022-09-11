9bc2975c4c08e3be98d8551d05504e0da2c6bed6
Kevin Liao <q85292542000@gmail.com>
2022-09-12 03:09:10 +0800

    feat: add gopls

--- init.lua
+++ init.lua
@@ -208,7 +208,7 @@ capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 local lspconfig = require("lspconfig")
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { "clangd", "rust_analyzer", "pyright", "tsserver" }
+local servers = { "clangd", "rust_analyzer", "pyright", "tsserver", "gopls" }
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
 		-- on_attach = my_custom_on_attach,
