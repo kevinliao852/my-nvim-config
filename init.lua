a147e0510b91b9bc83e6d9f4eef2cd632289ef99
Kevin Liao <q85292542000@gmail.com>
2022-09-19 03:26:44 +0800

    feat: change python formatter to black

--- init.lua
+++ init.lua
@@ -130,7 +130,7 @@ require("null-ls").setup({
 	sources = {
 		require("null-ls").builtins.formatting.stylua,
 		require("null-ls").builtins.formatting.prettier,
-		require("null-ls").builtins.formatting.autopep8,
+		require("null-ls").builtins.formatting.black,
 		require("null-ls").builtins.formatting.gofmt,
 		require("null-ls").builtins.diagnostics.eslint,
 		require("null-ls").builtins.diagnostics.pylint,
