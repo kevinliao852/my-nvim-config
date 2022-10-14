a612d107435e533c589cd3697647813e2bc8e1f5
Kevin Liao <q85292542000@gmail.com>
2022-10-14 23:53:20 +0800

    feat: add buffer switch mapping

--- init.lua
+++ init.lua
@@ -26,6 +26,8 @@ keymap("n", "<leader>w", ":w<CR>", opts)
 keymap("i", "<C-l>", "<Esc>", opts)
 keymap("v", "<C-l>", "<Esc>", opts)
 keymap("n", "<Esc><Esc>", ":noh<CR>", opts)
+keymap("n", "<space>[",":bprev<CR>", opts)
+keymap("n", "<space>]",":bnext<CR>", opts)
 
 keymap("n", "U", ":UndotreeToggle<CR>", opts)
 keymap("n", "<leader>tt", ":NERDTreeToggle<CR>", opts)
