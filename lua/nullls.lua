3fdd6a55b7b28199b2a905da5b9d8b96ec4bdae1
Kevin Liao <q85292542000@gmail.com>
2022-10-23 07:26:15 +0800

    feat: use prettierd instead

--- lua/nullls.lua
+++ lua/nullls.lua
@@ -7,7 +7,7 @@ end
 nullls.setup({
 	sources = {
 		require("null-ls").builtins.formatting.stylua,
-		require("null-ls").builtins.formatting.prettier,
+		require("null-ls").builtins.formatting.prettierd,
 		require("null-ls").builtins.formatting.black.with({
 			extra_args = { "-S" },
 		}),
