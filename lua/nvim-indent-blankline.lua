c02bf6da989d7c2bd998e3f54b86ec519fb5ed2a
Kevin Liao <q85292542000@gmail.com>
2023-07-13 12:27:12 +0800

    feat: make a decent indent blank

--- lua/nvim-indent-blankline.lua
+++ lua/nvim-indent-blankline.lua
@@ -1,19 +1,20 @@
 vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
+vim.cmd([[highlight IndentBlanklineIndent1 guibg=#1F1F28 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent2 guibg=#2A2A37 gui=nocombine]])
 
 vim.opt.list = true
 -- vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup({
-	space_char_blankline = " ",
+	char = "",
 	char_highlight_list = {
 		"IndentBlanklineIndent1",
 		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
 	},
+	space_char_highlight_list = {
+		"IndentBlanklineIndent1",
+		"IndentBlanklineIndent2",
+	},
+	show_trailing_blankline_indent = false,
 })
