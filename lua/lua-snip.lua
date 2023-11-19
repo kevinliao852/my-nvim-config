b7e796545b79d2074e9b6b4d2be224cec0c4ab84
Kevin Liao <q85292542000@gmail.com>
2023-11-19 17:31:16 +0800

    feat: extend ts snippet from js

--- lua/lua-snip.lua
+++ lua/lua-snip.lua
@@ -6,3 +6,7 @@ end
 -- require("luasnip.loaders.from_vscode").lazy_load()
 
 require("luasnip.loaders.from_vscode").load()
+
+-- set for javascript & typescript
+
+lua_snip.filetype_extend("typescript", { "javascript" })
