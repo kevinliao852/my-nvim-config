85324138446f17bb8c8b547b9166a79846e0b108
Kevin Liao <q85292542000@gmail.com>
2024-10-27 21:45:31 +0800

    ✨ Add neotest for go testing

--- lua/lazyvim.lua
+++ lua/lazyvim.lua
@@ -120,6 +120,38 @@ require("lazy").setup({
 	-- 	end,
 	-- },
 	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/neotest-go",
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"leoluz/nvim-dap-go",
+			-- Your other test adapters here
+		},
+		config = function()
+			-- get neotest namespace (api call creates or returns namespace)
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+
+					format = function(diagnostic)
+						local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
+			require("neotest").setup({
+				-- your neotest config here
+				adapters = {
+					require("neotest-go"), -- Registration
+				},
+			})
+		end,
+	},
+	{
 		"kevinliao852/nvim-folding",
 		config = function()
 			require("nvim-folding").setup()
