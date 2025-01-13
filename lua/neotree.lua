7385822d00452a0768fbd67de4030a6b64b5d87a
Kevin Liao <q85292542000@gmail.com>
2025-01-13 20:47:53 +0800

    🐛 Fix neotree config

--- lua/neotree.lua
+++ lua/neotree.lua
@@ -7,7 +7,6 @@ end
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-
 neotree.setup({
 	close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
 	popup_border_style = "rounded",
@@ -155,7 +154,9 @@ neotree.setup({
 				--".null-ls_*",
 			},
 		},
-		follow_current_file = false, -- This will find and focus the file in the active buffer every
+		follow_current_file = {
+			enabled = true,
+		}, -- This will find and focus the file in the active buffer every
 		-- time the current file is changed while the tree is open.
 		group_empty_dirs = false, -- when true, empty folders will be grouped together
 		hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -180,7 +181,9 @@ neotree.setup({
 		},
 	},
 	buffers = {
-		follow_current_file = true, -- This will find and focus the file in the active buffer every
+		follow_current_file = {
+			enabled = true,
+		}, -- This will find and focus the file in the active buffer every
 		-- time the current file is changed while the tree is open.
 		group_empty_dirs = true, -- when true, empty folders will be grouped together
 		show_unloaded = true,
