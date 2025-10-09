5f7ae2adfdd233e5d9e9babb3c34071db63137fc
Mao-Kai Lan <maokailan24@gmail.com>
2025-10-09 09:00:22 +0800

    ✏️ fix(typos): correct spelling mistakes in code comments (#72)
    Signed-off-by: Mao-Kai Lan <maokailan24@gmail.com>

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -19,7 +19,7 @@ for _, lsp in ipairs(servers) do
 		capabilities = capabilities,
 		handlers = {
 			["tailwindcss/getConfiguration"] = function(_, _, params, _, bufnr, _)
-				-- tailwindcss lang server waits for this repsonse before providing hover
+				-- tailwindcss lang server waits for this response before providing hover
 				vim.lsp.buf_notify(bufnr, "tailwindcss/getConfigurationResponse", { _id = params._id })
 			end,
 		},
